72e7211e64789ace50a1289e75452c7a70c75c18
Alexander <sashka0264@yandex.ru>
2019-03-02 16:33:54 +0300

    Внес правки в js

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,19 @@ var button = document.querySelector("#button");
 var modal = document.querySelector("#modal");
 var close = document.querySelector("#close");
 
-button.addEventListener("click", function(){
+// button.addEventListener("click", function start(){
+//     modal.classList.add("modal_active")
+// })
+
+// close.addEventListener("click", function end(){
+//     modal.classList.remove("modal_active")
+// })
+
+button.addEventListener("click", function start(){
     modal.classList.add("modal_active")
+    setTimeout(function end(){
+        modal.classList.remove("modal_active")}, 5000
+    )
 })
 
-close.addEventListener("click", function(){
-    modal.classList.remove("modal_active")
-})
+
